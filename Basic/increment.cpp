dbede36c41b6802003a6d00e6f5b1bd84dac97da
fadilachmad <ahmd.sufadil@gmail.com>
2024-09-16 18:20:45 +0700

    Basic C++

---
+++ Basic/increment.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int in;
+    in = 10;
+    cout << "nilai awal : " << in << endl;
+    cout << "nilai ++in : " << ++in << endl;
+    cout << "nilai hasil : " << in << endl;
+
+    in = 10;
+    cout << "nilai awal : " << in << endl;
+    cout << "nilai ++in : " << in++ << endl;
+    cout << "nilai hasil : " << in << endl;
+}
